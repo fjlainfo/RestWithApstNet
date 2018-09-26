0df17cc2c93c2c1dd3b1e65574b793592fbc728f
Francisco José de Lima Alves <fjlainfo@gmail.com>
2018-09-26 01:26:14 -0300

    Ajuste nas branchs

---
+++ RestWithApstNet - 03/RestWithApstNet/Controllers/PersonsController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestWithApstNet.Model;
+using RestWithApstNet.Services;
+
+namespace RestWithApstNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        private IPersonService _personService;
+
+        public PersonsController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_personService.FindAll());
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var person = _personService.FindById(id);
+            if (person == null) return NotFound();
+            return Ok(person);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return new ObjectResult(_personService.Create(person));
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return new ObjectResult(_personService.Update(person));
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _personService.Delete(id);
+            return NoContent();
+        }
+    }
+}
