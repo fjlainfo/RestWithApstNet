69e52665b7494cadcec01dbe71f96a6c6b0fc310
Francisco José de Lima Alves <fjlainfo@gmail.com>
2018-10-22 13:29:43 -0300

    implented migration parte 2

---
+++ RestWithApstNet - 05/RestWithApstNet/Business/Implementattions/PersonBusinessImpl.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using RestWithApstNet.Model;
+using RestWithApstNet.Model.Context;
+using RestWithApstNet.Repository;
+
+namespace RestWithApstNet.Business.Implementattions
+{
+    public class PersonBusinessImpl : IPersonBusiness
+    {
+        private IPersonRepository _repository;
+
+        public PersonBusinessImpl(IPersonRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Person Create(Person person)
+        {
+            return _repository.Create(person);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+        public List<Person> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        public Person FindById(long id)
+        {
+            return _repository.FindById(id);
+        }
+
+        public Person Update(Person person)
+        {
+            return _repository.Update(person);
+        }
+    }
+}
+
+
+
---
+++ RestWithApstNet - 06/RestWithApstNet/Controllers/PersonsController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestWithApstNet.Model;
+using RestWithApstNet.Business;
+
+namespace RestWithApstNet.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiVersion("1")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    //[ApiController]
+    public class PersonsController : ControllerBase
+    {
+        private IPersonBusiness _personBusiness;
+
+        public PersonsController(IPersonBusiness personService)
+        {
+            _personBusiness = personService;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_personBusiness.FindAll());
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var person = _personBusiness.FindById(id);
+            if (person == null) return NotFound();
+            return Ok(person);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return new ObjectResult(_personBusiness.Create(person));
+        }
+
+        // PUT api/values/5
+        [HttpPut("v1")]
+        public IActionResult Put([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return new ObjectResult(_personBusiness.Update(person));
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("v1")]
+        public IActionResult Delete(int id)
+        {
+            _personBusiness.Delete(id);
+            return NoContent();
+        }
+    }
+}
