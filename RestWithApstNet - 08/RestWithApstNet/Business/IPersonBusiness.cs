07d9bbc980cb2304238edd4b14a57bc2b835b432
Francisco José de Lima Alves <fjlainfo@gmail.com>
2018-10-25 01:22:18 -0300

    Migration

---
+++ RestWithApstNet - 08/RestWithApstNet/Business/IPersonBusiness.cs
@@ -0,0 +1,14 @@
+﻿using RestWithApstNet.Model;
+using System.Collections.Generic;
+
+namespace RestWithApstNet.Business
+{
+    public interface IPersonBusiness
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
